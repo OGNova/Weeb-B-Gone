543f7a9244fd9a6660b8587252d77528c8ed09fb
OGNova <jamooney2000@gmail.com>
2021-03-16 01:54:09 -0400

    I'm back, and more insane than ever.

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const BaseClient = require('./src/client/BaseClient');
 const init = require('./src/client/Init');
 const createLogger = require('./src/client/GlobalLogger');
 
+const myIntents = ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"];
+
 const client = new BaseClient({
   fetchAllMembers: true,
-  partials: ['MESSAGE', 'CHANNEL']
+  partials: ['MESSAGE', 'CHANNEL'],
+  ws: { intents: myIntents }
 });
 global.bot = client;
 
-init(client, process.env.PROD_TOKEN, true, true).catch(e => console.log(e));
+init(client, process.env.PROD_TOKEN).catch(e => console.log(e));
 createLogger(client);
